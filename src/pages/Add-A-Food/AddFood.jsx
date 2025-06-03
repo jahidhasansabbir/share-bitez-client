73472c41c0feb71c9cb812f0f9df50fbf5b67b16
jahidhasansabbir1 <jahidhasansabbir1@gmail.com>
2025-06-04 01:36:38 +0600

    added add food page

---
+++ src/pages/Add-A-Food/AddFood.jsx
@@ -0,0 +1,137 @@
+import axios from "axios";
+import React, { use } from "react";
+import { AuthContext } from "../../context/AuthContext";
+
+const AddFood = () => {
+  const {user} = use(AuthContext);
+  const {displayName, photoURL, email} = user;
+  const donor = { name: displayName , email: email, image: photoURL };
+    console.log(donor);
+  const handleAddFood = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const foodName = form.foodName.value;
+    const pickupLocation = form.pickupLocation.value;
+    const foodQuantity = form.foodQuantity.value;
+    const foodImage = form.foodImage.value;
+    const expiredDateTime = form.expiredDateTime.value;
+    const additionalNotes = form.additionalNotes.value;
+    const foodStatus = "available";
+    const food = {
+      foodName,
+      foodImage,
+      foodQuantity,
+      pickupLocation,
+      expiredDateTime,
+      additionalNotes,
+      donor,
+      foodStatus,
+    };
+    axios.post(`${import.meta.env.VITE_server}/foods`, food)
+      .then((res) => {
+        if(res.data.insertedId){
+            food._id=res.data.insertedId;
+            form.reset();
+        }
+      });
+  };
+  return (
+    <div className="w-11/12 mx-auto my-8">
+      <form className="fieldset" onSubmit={handleAddFood}>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          <div className="flex flex-col">
+            <label className="label">Food Name</label>
+            <input
+              required
+              type="text"
+              name="foodName"
+              className="input w-full"
+              placeholder="e.g: Cup-cake"
+            />
+          </div>
+
+          <div className="flex flex-col">
+            <label className="label">Food Image</label>
+            <input
+              required
+              type="text"
+              name="foodImage"
+              className="input w-full"
+              placeholder="Please enter photoURL"
+            />
+          </div>
+
+          <div className="flex flex-col">
+            <label className="label">Food Quantity</label>
+            <input
+              required
+              type="text"
+              name="foodQuantity"
+              className="input w-full"
+              placeholder="Enter food quantity"
+            />
+          </div>
+
+          <div className="flex flex-col">
+            <label className="label">Pickup Location</label>
+            <input
+              type="text"
+              required
+              name="pickupLocation"
+              className="input w-full"
+              placeholder="Enter pickup location"
+            />
+          </div>
+
+          <div className="flex flex-col">
+            <label className="label">Expired Date/Time</label>
+            <input
+              type="datetime-local"
+              required
+              name="expiredDateTime"
+              className="input w-full"
+              placeholder="Select date and time"
+            />
+          </div>
+
+          {/* <div className="flex flex-col">
+            <label className="label">Name</label>
+            <input
+              required
+              type="text"
+              name="name"
+              value={displayName}
+              disabled
+              className="input w-full"
+              placeholder=""
+            />
+          </div>
+          <div className="flex flex-col">
+            <label className="label">Email</label>
+            <input
+              required
+              type="email"
+              name="email"
+              value={email}
+              disabled
+              className="input w-full"
+              placeholder="Enter your email"
+            />
+          </div> */}
+        </div>
+        <div className="flex flex-col w-full">
+          <label className="label">Additional Notes</label>
+          <textarea
+            className="textarea h-24 w-full"
+            name="additionalNotes"
+            required
+            placeholder="Describe..."
+          ></textarea>
+        </div>
+        <button className="btn btn-neutral mt-4">Add</button>
+      </form>
+    </div>
+  );
+};
+
+export default AddFood;
