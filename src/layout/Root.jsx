289d4c8fe60834c5c7e2012b67dc71cd0303e9b0
jahidhasansabbir1 <jahidhasansabbir1@gmail.com>
2025-06-04 09:35:21 +0600

    added footer

--- src/layout/Root.jsx
+++ src/layout/Root.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import '../App.css'
 import { Outlet } from 'react-router';
 import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
 const Root = () => {
     return (
-        <div>
+        <div className='max-w-[1440px] mx-auto'>
             <Navbar></Navbar>
             <Outlet/>
+            <Footer></Footer>
         </div>
     );
 };
