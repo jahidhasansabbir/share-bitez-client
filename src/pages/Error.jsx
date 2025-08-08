18b17c892ebf2a903ff22440a4d8dac02390b8d4
jahidhasansabbir1 <jahidhasansabbir1@gmail.com>
2025-08-08 21:56:19 +0600

    fix: fix 404 error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,17 +1,29 @@
-import React from "react";
 import { Link } from "react-router";
+import { FaExclamationTriangle } from "react-icons/fa";
 
 const Error = () => {
-  return (
+    return (
     <div className="min-h-screen flex items-center justify-center bg-base-200 text-base-content px-4">
-      <div className="text-center space-y-6">
-        <h1 className="text-6xl font-bold text-primary">404</h1>
-        <p className="text-2xl font-semibold">Page Not Found</p>
-        <p className="text-base text-gray-500 dark:text-gray-400">
-          Sorry, the page you are looking for doesn’t exist or has been moved.
+      <div className="text-center space-y-6 max-w-xl">
+        <div className="inline-flex items-center gap-4 animate-pulse">
+          <FaExclamationTriangle className="text-6xl text-red-500" />
+          <h1 className="text-6xl font-extrabold text-primary">404</h1>
+        </div>
+
+        {/* Message */}
+        <p className="text-3xl font-bold text-gray-800 ">
+          Page Not Found
         </p>
-        <Link to="/" className="btn btn-primary">
-          Go Home
+        <p className="text-base text-gray-600">
+          Sorry, the page you're looking for doesn’t exist, has been moved, or removed.
+        </p>
+
+        {/* Button */}
+        <Link
+          to="/"
+          className="btn bg-blue-600 border-none btn-wide text-base text-white shadow-md hover:shadow-xl transition duration-300"
+        >
+          Go Back Home
         </Link>
       </div>
     </div>
